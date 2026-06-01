9d4905c7f5676b0e058c535338d977ced3db4aae
Anushri Saren <163124793+Anushri202406@users.noreply.github.com>
2026-06-01 17:11:06 +0530

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,16 @@
+-- Students table
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT,
+    city VARCHAR(50)
+);
+
+-- Marks table  
+CREATE TABLE marks (
+    mark_id INT PRIMARY KEY,
+    student_id INT,
+    subject VARCHAR(30),
+    marks INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id)
+);
